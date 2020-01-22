a5f42328af34c4d859ab9564b1ad416009616b18
James Dindi <dindijjames@gmail.com>
2020-01-22 15:30:32 +0300

    Add Todo and TodoForm components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,  { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for lunch',
+      isCompleted: false
+    },
+    {
+      text: 'Build really cool todo app',
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        { todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
