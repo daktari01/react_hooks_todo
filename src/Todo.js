88939aa20867e6b3ba116ace212f01d549eb85e0
James Dindi <dindijjames@gmail.com>
2020-01-22 15:50:11 +0300

    Delete the todos

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function Todo({ todo, index, completeTodo }) {
+function Todo({ todo, index, completeTodo, removeTodo }) {
 
   return (
     <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
       { todo.text }
       <div>
         <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>X</button>
       </div>
     </div>
   )
